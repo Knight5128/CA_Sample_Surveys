65a1bb719f1b6393fbdefc7a8d430a812efa9b70
LIU JIANGZHE <liujz_5128@163.com>
2024-11-06 00:14:24 -0800

    确定了研究数据集 & 初步完成报告第一部分 by ljz (#4)
    * add some basic utils functions #1
    
    * Added 4 sampling methods & simulation & basic visualization & simulation visualization
    
    * 确定了研究数据集 & 初步完成报告导入部分

--- main.R
+++ main.R
@@ -1,17 +1,60 @@
 # nolint: object_name_linter
 #installation of necessary packages
 source("utils.R")
-source("visualization.R")
 library(tidyverse)
 library(ggplot2)
+library(ipumsr)
+
+####################################################################################
+#——————————————————————————————Research Description————————————————————————————————#
+####################################################################################
 
 
 
-#import data
-raw_df <- read.csv(PATH)
 
 
-#Data Visualization
+####################################################################################
+#———————————————————————————————————Data Import————————————————————————————————————#
+####################################################################################
+
+#----------DO NOT try to import the .csv file directly. It will not work.----------#
+#----------You need to import the data using the ipumsr package----------#
+#----------Also, DO NOT change the file name of <*.dat>----------#
+
+ddi <- read_ipums_ddi("./data/usa_222016_01.xml")
+rawdf_222016 <- read_ipums_micro(ddi)
+
+#split data into 3 years: 2016, 2020, 2022
+
+rawdf_2016 <- rawdf_222016[rawdf_222016$YEAR == 2016,]
+rawdf_222016 <- rawdf_222016[rawdf_222016$YEAR != 2016,]
+rawdf_2020 <- rawdf_222016[rawdf_222016$YEAR == 2020,]
+rawdf_222016 <- rawdf_222016[rawdf_222016$YEAR != 2020,]
+rawdf_2022 <- rawdf_222016[rawdf_222016$YEAR == 2022,]
+rawdf_222016 <- rawdf_222016[rawdf_222016$YEAR != 2022,]
+
+#check if there's any NA in each df
+colSums(is.na(rawdf_2016))
+colSums(is.na(rawdf_2020))
+colSums(is.na(rawdf_2022))
+#No NA in any df!!!
+
+
+#Select only relevant columns that are relevant to our study:
+#i.e. keep what we care about
+rawdf_2016 <- rawdf_2016 %>% select(YEAR, SERIAL, CBSERIAL, HHWT, STATEFIP, PERNUM）
+
+                                    
+#find plausible auxiliary variable for Ratio Estimation 
+
+
+
+
+                                    
+
+####################################################################################
+#————————————————————————————————Data Visualization————————————————————————————————#
+####################################################################################
 # For continuous variable (tarv_1)
 plots1 <- plot_continuous_distribution(data, "your_var_name", "strata_column")
 plots1$density  # View density plot
@@ -25,7 +68,9 @@ plots2$stratified  # View stratified proportions
 
 
 
-#Sampling Simulation
+####################################################################################
+#——————————————————————————————Sampling Simulation—————————————————————————————————#
+####################################################################################
 # Run simulation
 sim_results <- compare_sampling_methods(
   data = target_poplation_df,
